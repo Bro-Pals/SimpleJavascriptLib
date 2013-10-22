3d221fea660f17c48c1f876196c062af646dbb28
Kevin <kprehn01@gmail.com>
2013-10-22 18:57:45 -0500

    Simple collision and physics!

--- SimpleLibrary/physics.js
+++ SimpleLibrary/physics.js
@@ -1,3 +1,13 @@
+// ~100 line physics code is super simple!
+
+var blockArray = new Array(); // an array to hold all the blocks
+
+// update all the blocks in the blockArray
+function updateBlocks() {
+	for (var i=0; i<blockArray.length; i++) {
+		blockArray[i].update();
+	}
+}
 
 /* The basic block used in physics for this library
  * x and y are for position of the block
@@ -5,6 +15,8 @@
  */
 var Block=Block;
 function Block(_x, _y, _width, _height) {
+	// add the block to the blockArray
+	blockArray[(blockArray.length)] = this;
 	// the cubes's basic values
 	this.x = _x;
 	this.y = _y;
@@ -33,6 +45,37 @@ function Block(_x, _y, _width, _height) {
 	// Later: Make it check for collisions
 	this.update=update;
 	function update() {
+		// check for collisions with other blocks
+		
+		for (var i=0; i<blockArray.length; i++) {
+			if (blockArray[i]!=this) {
+				// check the X axis
+				if (this.willIntersectsX(blockArray[i])) {
+					if ((this.velocity[0] > 0) && // if it was moving right
+					    (this.x+this.width+this.velocity[0] > blockArray[i].x)) {
+							this.x = blockArray[i].x-this.width;
+						} else if ((this.velocity[0] < 0) && // if it was moving left
+					    (this.x+this.velocity[0] < blockArray[i].x+blockArray[i].width)) {
+							this.x = blockArray[i].x+blockArray[i].width;
+						}
+					this.acceleration[0] = 0;
+					this.velocity[0] = 0;
+				}
+				// check the Y axis 
+				if (this.willIntersectsY(blockArray[i])) {
+					if ((this.velocity[1] > 0) && // if it was moving down
+					    (this.y+this.height+this.velocity[1] > blockArray[i].y)) {
+							this.y = blockArray[i].y-this.height;
+						} else if ((this.velocity[1] < 0) && // if it was moving up
+					    (this.y+this.velocity[1] < blockArray[i].y+blockArray[i].height)) {
+							this.y = blockArray[i].y+blockArray[i].height;
+						}
+					this.acceleration[1] = 0;
+					this.velocity[1] = 0;
+				}
+			}
+		}
+		
 		// update the velocity with acceleration
 		this.velocity = [this.velocity[0]+this.acceleration[0],
 						 this.velocity[1]+this.acceleration[1]];
@@ -40,5 +83,25 @@ function Block(_x, _y, _width, _height) {
 		// update the position with acceleration
 		this.x = this.x+this.velocity[0];
 		this.y = this.y+this.velocity[1];
+	}
+	
+	/* see if the block will collide with the other block if
+	 * you add it's x velocity to the x position */
+	this.willIntersectsX=willIntersectsX;
+	function willIntersectsX(otherBlock) {
+		return ((otherBlock.x+otherBlock.width > this.x+this.velocity[0]) &&
+			(otherBlock.x < this.x+this.width+this.velocity[0]) &&
+			(otherBlock.y+otherBlock.height > this.y) && 
+			(otherBlock.y < this.y+this.height));
+	}
+	
+	/* see if the block will collide with the other block if
+	 * you add it's y velocity to the y position */
+	this.willIntersectsY=willIntersectsY;
+	function willIntersectsY(otherBlock) {
+		return ((otherBlock.x+otherBlock.width > this.x) &&
+			(otherBlock.x < this.x+this.width) && 
+			(otherBlock.y+otherBlock.height > this.y+this.velocity[1]) && 
+			(otherBlock.y < this.y+this.height+this.velocity[1]));
 	}
 }
