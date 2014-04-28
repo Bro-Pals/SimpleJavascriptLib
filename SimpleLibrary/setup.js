9ac8f8c7130dd12147ecf9edfb06e5b7bcf1f199
Kevin <kprehn01@gmail.com>
2014-04-28 18:53:56 -0500

    Adding new block.js file and (finally) added different screens in the library

--- SimpleLibrary/setup.js
+++ SimpleLibrary/setup.js
@@ -1,4 +1,50 @@
 
+/* This uses different "sceens" to organize the differents screens in a game.
+
+	each scene is set up as the following
+	
+	var MY_SCREEN = new Array();
+	MY_SCREEN[0] = function update() { //.. update code }
+	MY_SCREEN[1] = function render() { //.. render code }
+	MY_SCREEN[2] = function onEnter() { //.. code that runs when this scene is first made the active screen }
+	MY_SCREEN[3] = function onExit() { //.. code that runs when this scene is being replaced and exited }
+
+	you can then use setScreen(MY_SCREEN); to make it the "active" scene that will be iterated through
+	
+*/
+
+// the active screen that is being ran
+var activeScreen = null;
+
+// change the current scene
+function setScreen(_screen) {
+	if (activeScreen != null) {
+		activeScreen[3](); // exit function on the previous screen
+	}
+	activeScreen = _screen;
+	activeScreen[2](); // enter function for new screen
+}
+
+// game looping stuff
+var start, diff;
+var fps = 30;
+var waitBetweenFrames = 1000/fps;
+
+// this function repeats at a constant fps when called
+function mainLoop() {
+	start = (new Date()).getTime();
+	activeScreen[0](); // update
+	
+	activeScreen[1](); // render
+	
+	diff = (new Date()).getTime() - start;
+	if (waitBetweenFrames - diff > 0) {
+		setTimeout(mainLoop, waitBetweenFrames - diff);
+	} else {
+		mainLoop();
+	}
+}
+
 // canvas variables
 var canvasContext;
 var canvasReference;
